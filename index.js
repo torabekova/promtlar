cce81ef775f97401a09182cd0f2acc054524be2c
unknown <faridatorabekova6@gmail.com>
2024-06-28 16:06:26 +0300

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+
+
+
+// var email = prompt("emailingizni kirgizing")
+    
+
+// var emailhidden =  email.slice(3, -10);
+
+// var hiddenemail = email.replace(emailhidden, "****" );
+// console.log(hiddenemail);
+
+
+
+
+var text = prompt ("matin kiriting");
+
+var changetext = prompt ("matin");
+
+var newtext = prompt ("new text enter")
+
+var chenchedtext =  text.replace(changetext, newtext )
+
+console.log(chenchedtext);
+
+var resulttext = chenchedtext.length
+console.log(resulttext);
+
+var colculetarray = chenchedtext.split(" ")
+
+console.log(colculetarray.length);
+
+
+
+
---
+++ practic.js
@@ -0,0 +1,10 @@
+// var text = prompt("malumot kirit")
+// var index = prompt("nimadir kirit")
+
+// var index = parseFloat(index);
+
+// if(text.charCodeAt(index) >= 65 && text.charCodeAt(index) <= 90 ){
+//     console.log("appercase");
+// }else if( text.charCodeAt(index) >= 90 && text.charCodeAt(index) <= 128 ){
+//     console.log("lowercase");
+// }
